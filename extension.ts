16db0cd961ef027aa4fb8aba3bd89acf2caf6e40
David <dkk5396@psu.edu>
2025-02-04 16:13:23 -0500

    first commit

---
+++ extension.ts
@@ -0,0 +1,43 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+//const vscode = require('vscode');
+
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+export function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	//console.log('Congratulations, your extension "deepseek-for-vs-code" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable = vscode.commands.registerCommand('deepseek-for-vs-code.start', () => {
+        const panel = vscode.window.createWebviewPanel(
+            'deepChat',
+            'Deep Seek Chat',
+            vscode.ViewColumn.One,
+            { enableScripts: true }
+        )
+
+        //panel.webview.html = getWebviewContent();
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
